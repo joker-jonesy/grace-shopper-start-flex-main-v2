818c94ef5568b9ec3bd7de496e10d12732a85898
cdriesner <driesnercalvin@gmail.com>
2023-06-05 10:23:58 -0700

    fixed webpack warnings

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
+import logger from 'redux-logger';
 import auth from './auth';
 import cart from './cart';
 
-const reducer = combineReducers({
-  auth,
-  cart
-});
-
 const store = configureStore({
+  middleware: (defaultMiddleware)=> defaultMiddleware().concat(logger),
   reducer:{
     auth: auth,
     cart: cart
@@ -15,4 +12,5 @@ const store = configureStore({
 });
 
 export default store;
-
+export * from './auth';
+export * from './cart';
