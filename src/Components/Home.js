41dc77f20cc47e5950b1cabb96b5899153808f03
Scott Weaver <scott@weaverscott.com>
2023-06-06 14:59:03 -0500

    removed test component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,5 @@
 import React from "react"
 import { useSelector, useDispatch } from "react-redux"
-import { logout } from "../store"
-import { Link } from "react-router-dom"
-import UserDetails from "./UserDetails"
 
 const Home = () => {
   const { auth } = useSelector((state) => state)
@@ -11,9 +8,7 @@ const Home = () => {
     <div className="flex flex-col items-center">
       <h1>Home</h1>
       {auth.username ? <div>Welcome {auth.username}!!</div> : <></>}
-      <div className="w-1/2">
-        <UserDetails user={auth} />
-      </div>
+      <div className="w-3-4"></div>
     </div>
   )
 }
