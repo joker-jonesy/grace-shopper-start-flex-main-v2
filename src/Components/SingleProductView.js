be12e2506104ccbaf712a34cb1a704fc18956101
Julian Bombard <jpbombard@yahoo.com>
2023-06-08 16:21:41 -0400

    changed input # to dropdown selector

--- src/Components/SingleProductView.js
+++ src/Components/SingleProductView.js
@@ -25,10 +25,17 @@ const SingleProductView = () => {
       <p>{product.description}</p>
       <h3>{product.price}</h3>
       <form onSubmit={addToCart}>
-        <input
+        {/* <input
           value={amount}
           onChange={(event) => setAmount(event.target.value)}
-        />
+        /> */}
+        <select>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
         <button>Add to Cart</button>
       </form>
     </div>
