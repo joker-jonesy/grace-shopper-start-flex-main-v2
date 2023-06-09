a8d0f77c53434874b1b72fb7c95671e91f71e48d
Julian Bombard <jpbombard@yahoo.com>
2023-06-09 16:33:16 -0400

    removed auth verifaction for add to cart button

--- src/Components/AddToCartButton.js
+++ src/Components/AddToCartButton.js
@@ -1,20 +1,15 @@
 import React from "react"
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { addToCart } from "../store";
 
 const AddToCartButton = (props) => {
-    const { auth } = useSelector(state => state)
     const dispatch = useDispatch()
     return (        
-            <>{
-                auth.id ? (
-                    <div className="flex flex-row-reverse w-full p-2">
-                        <button  onClick={() => dispatch(addToCart(props))} className="btn-secondary btn-sm btn text-base-300">
-                            Add to Cart
-                        </button>
-                    </div>
-                ): null
-            }</>          
+                <div className="flex flex-row-reverse w-full p-2">
+                    <button  onClick={() => dispatch(addToCart(props))} className="btn-secondary btn-sm btn text-base-300">
+                        Add to Cart
+                    </button>
+                </div>
             )
 }
 
