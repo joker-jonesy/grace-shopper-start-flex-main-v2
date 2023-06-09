ec9bc0424fd2157bcda0a2800fa73b7e72189a1e
Julian Bombard <jpbombard@yahoo.com>
2023-06-09 12:05:48 -0400

    added thunks for adding and removing from cart

--- src/Components/SingleProductView.js
+++ src/Components/SingleProductView.js
@@ -19,24 +19,28 @@ const SingleProductView = () => {
   }
 
   return (
-    <div>
-      <h1>{product.name}</h1>
+    <div className="card card-compact sm:card-normal glass m-4 w-64">
       <img src= {product.imageURL}/>
-      <p>{product.description}</p>
-      <h3>{product.price}</h3>
+      <h1 className="text-lg font-bold">{product.name}</h1>
+      <p className='text-sm'>{product.description}</p>
+      <span className="badge badge-ghost">
+        <span className="text-lg font-bold">$</span><span className="font-bold">{product.price}</span>
+      </span>
       <form onSubmit={addToCart}>
         {/* <input
           value={amount}
           onChange={(event) => setAmount(event.target.value)}
         /> */}
-        <select>
+        <select className="select select-bordered w-full max-w-xs">
           <option value={1}>1</option>
           <option value={2}>2</option>
           <option value={3}>3</option>
           <option value={4}>4</option>
           <option value={5}>5</option>
         </select>
-        <button>Add to Cart</button>
+        <button className="btn-secondary btn-sm btn mr-2">
+          Add to Cart
+        </button>
       </form>
     </div>
   )
