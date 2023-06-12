cb0efdbd1f071b53471557c8b999013016d6c276
Scott Weaver <scott@weaverscott.com>
2023-06-12 14:08:20 -0500

    userId

--- src/Components/ui/WishListButton.js
+++ src/Components/ui/WishListButton.js
@@ -4,23 +4,20 @@ import { useSelector } from 'react-redux'
 import { useToast } from '../../hooks/useToast'
 
 function WishListButton() {
-    const { user } = useSelector(state => state.auth)
+    const { id } = useSelector(state => state.auth)
     const { notify } = useToast()
     const [isPending, setIsPending] = React.useState(false)
     const [isWishListed, setIsWishListed] = React.useState(false)
     const onClick = () => {
-        if (!user) return notify("You must be logged in to add to wishlist", "error");
+        console.log(id)
+        if (!id) return notify("You must be logged in to add to wishlist", "error");
         setIsPending(true)
-        console.log("here")
-
-
-
         setIsWishListed(!isWishListed)
         //do api call to add to wishlist
     }
     return (
         <button className={`btn btn-circle btn-sm hover:scale-125 ${isWishListed ? "bg-red-600" : "bg-gray-400"}`} onClick={() => onClick()}>
-            <HeartIcon size={24} className={isPending ? "animate-spin" : ""} />
+            <HeartIcon size={24} className={isPending ? "animate animate-spin" : ""} />
         </button>
     )
 }
