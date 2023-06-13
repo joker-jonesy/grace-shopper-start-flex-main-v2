c9cb99dfdd824adf8d843cbeffc4d5c6ea6d4e76
chee86j <jeffreychee86@gmail.com>
2023-06-13 16:57:54 -0400

    Product Not Found Page Done

--- src/Components/SingleProductView.js
+++ src/Components/SingleProductView.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchSingleProduct } from "../store/singleProduct"
-import { useParams } from "react-router-dom"
+import { useParams, useNavigate, NavLink } from "react-router-dom"
 import AddToCartButton from "./AddToCartButton"
 import { createReview } from "../store"
 
@@ -9,13 +9,14 @@ const SingleProductView = () => {
   const [quantity, setQuantity] = useState(1)
   const [description, setDescription] = useState("")
   const [rating, setRating] = useState()
-  const product = useSelector(state => state.singleProduct)
+  const product = useSelector((state) => state.singleProduct)
   const dispatch = useDispatch()
   const { id } = useParams()
+  const navigate = useNavigate()
 
   useEffect(() => {
     dispatch(fetchSingleProduct(id))
-  }, [dispatch])
+  }, [dispatch, id])
 
   const addToCart = (event) => {
     event.preventDefault()
@@ -25,10 +26,7 @@ const SingleProductView = () => {
   const handleSubmit = (event) => {
     event.preventDefault()
 
-    const newReview = (
-      description, 
-      rating
-    )
+    const newReview = (description, rating)
 
     dispatch(createReview(newReview))
 
@@ -38,37 +36,59 @@ const SingleProductView = () => {
 
   const starArr = Array.from(Array(5).keys())
 
-  return (
+  const handleReturn = () => {
+    navigate(-1) // to go back to the previous page
+  }
+
+  return !product.id ? (
+    <>
+      <h1 className="bg-gradient-to-r from-success to-accent bg-clip-text text-9xl font-extrabold text-transparent">
+        Product Not Found
+      </h1>
+      <NavLink
+        to={"/"}
+        className={({ isActive, isPending }) =>
+          `text-md btn-ghost btn normal-case ${isActive ? "btn-active" : ""}`
+        }
+      >
+        Return to Previous Page
+      </NavLink>
+    </>
+  ) : (
     <div>
-      <div className="card card-compact sm:card-normal glass m-4 w-64">
-        <img src= {product.imageURL}/>
+      <div className="card glass card-compact m-4 w-64 sm:card-normal">
+        <img src={product.imageURL} />
         <h1 className="text-lg font-bold">{product.name}</h1>
-        <p className='text-sm'>{product.description}</p>
+        <p className="text-sm">{product.description}</p>
         <span className="badge badge-ghost">
-          <span className="text-lg font-bold">$</span><span className="font-bold">{product.price}</span>
+          <span className="text-lg font-bold">$</span>
+          <span className="font-bold">{product.price}</span>
         </span>
         <form onSubmit={addToCart}>
           <input
             value={quantity}
             onChange={(event) => setQuantity(event.target.value)}
           />
-          <AddToCartButton product={product} quantity={parseInt(quantity)}/>
+          <AddToCartButton product={product} quantity={parseInt(quantity)} />
         </form>
       </div>
-      <div>Leave a Review
+      <div>
+        Leave a Review
         <form onSubmit={handleSubmit}>
-          <label>Description
+          <label>
+            Description
             <input
-            value={description}
-            onChange={(event) => setDescription(event.target.value)}
+              value={description}
+              onChange={(event) => setDescription(event.target.value)}
             />
           </label>
-          <label>Star Rating
+          <label>
+            Star Rating
             {/* <input
             value={rating}
             onChange={(event) => setRating(event.target.value)}
             /> */}
-            <select className="select select-bordered w-full max-w-xs">
+            <select className="select-bordered select w-full max-w-xs">
               <option value={1}>1</option>
               <option value={2}>2</option>
               <option value={3}>3</option>
@@ -76,12 +96,12 @@ const SingleProductView = () => {
               <option value={5}>5</option>
             </select>
           </label>
-          <button disabled = {description === "" || rating === ""}type="submit">Submit</button>
+          <button disabled={description === "" || rating === ""} type="submit">
+            Submit
+          </button>
         </form>
       </div>
     </div>
-      
-
   )
 }
 export default SingleProductView
