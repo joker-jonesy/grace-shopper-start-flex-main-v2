32ab7101b6797bc45f3a1e3616c3d05b25d8e729
cdriesner <driesnercalvin@gmail.com>
2023-06-13 13:04:46 -0700

    styled order page

--- src/Components/pages/OrdersPage.js
+++ src/Components/pages/OrdersPage.js
@@ -21,7 +21,6 @@ const Orders = () => {
         },
       })
       setOrders(response.data);
-      console.log(response);
     }catch (error){
       console.log(error)
     }
@@ -31,6 +30,8 @@ const Orders = () => {
     fetchOrders()
   },[auth])
 
+  console.log(orders);
+
   const LogedIn = () => {
     
     return (
@@ -38,30 +39,77 @@ const Orders = () => {
         <thead>
           <tr>
             <th>Order Number</th>
-            <th>products</th>
+            <th>Products</th>
+            <th>Date Placed</th>
+            <th>Status</th>
           </tr>
         </thead>
         <tbody>
           {orders.map((order) => {
             return (
-              <tr>
+              <tr key={uuidv4()}>
                 <td>{order.lookUpId}</td>
                 <td>{order.lineItems.length}</td>
+                <td>{order.createdAt}</td>
+                <td>shipped</td>
                 <td>
                   <button
                     className="btn"
                     onClick={() => window[order.id].showModal()}
                   >
-                    open modal
+                    View Details
                   </button>
                   <dialog id={order.id} className="modal">
-                    <form method="dialog" className="modal-box">
-                      <h1>Order Number {order.lookUpId}</h1>
-                      <h2></h2>
-                      <div className="modal-action">
-                        <button className="btn">Close</button>
-                      </div>
-                    </form>
+                    <div className="text-white">
+                      <form
+                        method="dialog"
+                        className="modal-box"
+                        id="myCoolBackground"
+                      >
+                        <div className="m-2 flex">
+                          <div className="m-2 rounded-lg p-6 backdrop-blur">
+                            <h1>Order Number {order.lookUpId}</h1>
+                            <h2>
+                              Placed By {order.firstName} {order.lastName}
+                            </h2>
+                            <h2>Created at: {order.createdAt}</h2>
+                          </div>
+                          <div className="m-2 rounded-lg p-6 backdrop-blur">
+                            <h1>Shippng To</h1>
+                            <h2>{order.street}</h2>
+                            <h2>
+                              {order.city}, {order.state} {order.zip}
+                            </h2>
+                            <h2>Arriving in: 2 days</h2>
+                          </div>
+                        </div>
+                        <h1 className="mx-auto text-lg">Order Items</h1>
+                        <div>
+                          {order.lineItems.map((item) => {
+                            return (
+                              <div key={uuidv4()} className="card card-side m-2 shadow-xl backdrop-blur">
+                                <figure>
+                                  <img
+                                    src={item.product.imageURL}
+                                    alt={item.product.name}
+                                  />
+                                </figure>
+                                <div className="card-body">
+                                  <h2 className="card-title">
+                                    {item.product.name}
+                                  </h2>
+                                  <h3>Quantity {item.quantity}</h3>
+                                  <h3>{item.product.price}</h3>
+                                </div>
+                              </div>
+                            )
+                          })}
+                        </div>
+                        <div className="modal-action">
+                          <button className="btn">Close</button>
+                        </div>
+                      </form>
+                    </div>
                   </dialog>
                 </td>
               </tr>
